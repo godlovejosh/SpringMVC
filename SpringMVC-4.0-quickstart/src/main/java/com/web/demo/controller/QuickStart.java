693be65f918c5694d72c2a4977b0f0f137f8121c
wuxing <lfq@500mi.com>
2016-08-24 13:54:51 +0800

    demo

---
+++ SpringMVC-4.0-quickstart/src/main/java/com/web/demo/controller/QuickStart.java
@@ -0,0 +1,29 @@
+package com.web.demo.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by wuxing on 16/8/23.
+ */
+@Controller
+public class QuickStart {
+
+    private final Log logger = LogFactory.getLog(QuickStart.class);
+
+    @RequestMapping(value="/",method = RequestMethod.HEAD)
+    public String head() {
+        return "quickStart.jsp";
+    }
+
+    @RequestMapping(value={"/index","/"},method = RequestMethod.GET)
+    public String index(Model model) throws Exception {
+        logger.info("==========processed by index==========");
+        model.addAttribute("msg", "QuickStart");
+        return "quickStart.jsp";
+    }
+}
